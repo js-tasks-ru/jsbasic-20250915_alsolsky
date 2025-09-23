c5a472bdc73f25dedb670b86352b9e9f7af40fcd
asolsky <alsolsky@yandex.ru>
2025-09-23 23:02:05 +0300

    Решение задачи Усечение строки

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -3,11 +3,4 @@ function checkSpam(str) {
   let lowString = str.toLowerCase();
   return lowString.includes('1xbet') || lowString.includes('xxx');  
 }
-checkSpam('1XbeT now'); // true
-checkSpam('free xxxxx'); // true
-checkSpam('innocent rabbit'); // false
 
-
-console.log(checkSpam('1XbeT now')); // true
-console.log(checkSpam('free xxxxx')); // true
-console.log(checkSpam('innocent rabbit')); // false 
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,13 @@
 function truncate(str, maxlength) {
   // ваш код...
+   
+    if (str.length > maxlength) {
+        return str.slice(0, maxlength -1) + '…'
+    } else {
+        return str
+  }
 }
+
+
+    
+
