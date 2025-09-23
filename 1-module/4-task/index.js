bdd8bfc6349bca2f1b76b35774a99282d3c6fb00
asolsky <alsolsky@yandex.ru>
2025-09-23 20:52:58 +0300

    Решение задачи Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,13 @@
 function checkSpam(str) {
   // ваш код...
+  let lowString = str.toLowerCase();
+  return lowString.includes('1xbet') || lowString.includes('xxx');  
 }
+checkSpam('1XbeT now'); // true
+checkSpam('free xxxxx'); // true
+checkSpam('innocent rabbit'); // false
+
+
+console.log(checkSpam('1XbeT now')); // true
+console.log(checkSpam('free xxxxx')); // true
+console.log(checkSpam('innocent rabbit')); // false 
