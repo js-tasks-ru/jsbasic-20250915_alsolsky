867d6880a7506e0d14e8bd1beb915f96bb278200
asolsky <alsolsky@yandex.ru>
2025-10-07 13:11:28 +0300

    Решение задачи Определите, пуст ли объект

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,16 @@
 function isEmpty(obj) {
   // ваш код...
+  for (let key in obj) {
+    // если тело цикла начнет выполняться - значит в объекте есть свойства
+    return false;
+  }
+  return true;
 }
+
+let schedule = {};
+
+alert(isEmpty(schedule)); // true
+
+schedule["8:30"] = "подъём";
+
+alert(isEmpty(schedule)); // false
