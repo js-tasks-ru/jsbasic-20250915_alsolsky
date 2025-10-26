ff4701837908a7273a8f04c50f791861b132d691
asolsky <alsolsky@yandex.ru>
2025-10-26 21:38:09 +0300

    Решение задачи Таблица с удаляемыми строками

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,44 @@
  *
  */
 export default class UserTable {
+
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.createTable();  
   }
+
+  createTable() {
+    const table = document.createElement('table');
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');    
+    thead.innerHTML = `
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    `;  
+    for (const row of this.rows) {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button>X</button></td>
+      `;      
+      const deleteButton = tr.querySelector('button');  
+      deleteButton.addEventListener('click', () => {
+        tr.remove();
+      });
+      tbody.appendChild(tr);
+    } 
+    table.appendChild(thead);
+    table.appendChild(tbody);    
+    return table;
+  } 
+  
+  
 }
