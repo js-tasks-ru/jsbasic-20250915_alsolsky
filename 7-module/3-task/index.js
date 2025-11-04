5694944d94ee3306cb384232ed9a34cb2c38010d
asolsky <alsolsky@yandex.ru>
2025-11-04 19:48:57 +0300

    Решение задачи Учебный проект: Пошаговый слайдер, часть 1

--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,61 @@
+
+
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+    this.elem = this.render();
+    this.initEvents();
+
+    this.updateSlider();
+
+
+  }
+  render() {
+    const slider = document.createElement('div');
+    slider.className = 'slider';  
+    slider.innerHTML = `
+      <div class="slider__thumb" style="left: 0%;">
+        <span class="slider__value">0</span>
+      </div>
+      <div class="slider__progress" style="width: 0%;"></div>
+      <div class="slider__steps">
+        ${'<span></span>'.repeat(this.steps)}
+      </div>
+    `;
+    return slider;  
+
+
+}
+  initEvents() {
+    this.elem.addEventListener('click', (event) => {
+      const rect = this.elem.getBoundingClientRect(); 
+      const clickX = event.clientX - rect.left;
+      const relativeClickX = clickX / rect.width;
+      const segments = this.steps - 1;  
+      const newValue = Math.round(relativeClickX * segments);
+
+      this.value = newValue;
+      this.updateSlider();  
+      this.elem.dispatchEvent(new CustomEvent('slider-change', {
+        detail: this.value,
+        bubbles: true
+      }));
+    });
+  }
+  updateSlider() {
+    const percentage = (this.value / (this.steps - 1)) * 100;
+    const thumb = this.elem.querySelector('.slider__thumb');
+    const progress = this.elem.querySelector('.slider__progress');
+    const valueDisplay = this.elem.querySelector('.slider__value');
+    const stepsSpans = this.elem.querySelectorAll('.slider__steps span');
+    thumb.style.left = `${percentage}%`;
+    progress.style.width = `${percentage}%`;
+    valueDisplay.textContent = this.value;  
+    stepsSpans.forEach((span, index) => {
+      span.classList.toggle('slider__step-active', index === this.value);
+    });
   }
 }
+
